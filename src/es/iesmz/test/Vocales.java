389c0e74a13b0ae32a15eb82f261c016e7e9ba8f
OmarLlinares <llinares.omar@gmail.com>
2024-03-25 09:53:12 +0100

    Initial commit

---
+++ src/es/iesmz/test/Vocales.java
@@ -0,0 +1,16 @@
+package es.iesmz.test;
+
+public class Vocales {
+
+    public static int contarVocales(String cadena){
+        int contador=0;
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = Character.toLowerCase(cadena.charAt(i));
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+}
---
+++ src/es/iesmz/test/VocalesTest.java
@@ -0,0 +1,6 @@
+package es.iesmz.test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VocalesTest {
+
+}
