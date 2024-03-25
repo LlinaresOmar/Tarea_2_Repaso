8d791cc7d03453dc14b56cdb99647baeb6a05ce7
OmarLlinares <llinares.omar@gmail.com>
2024-03-25 09:57:28 +0100

    3º Commit Ultimo Test

--- src/es/iesmz/test/VocalesTest.java
+++ src/es/iesmz/test/VocalesTest.java
@@ -9,4 +9,23 @@ class VocalesTest {
         assertEquals(4, Vocales.contarVocales("Hola Mundo"));
     }
 
+    @Test
+    public void contarVocales2(){
+        assertEquals(8, Vocales.contarVocales("Esto Es una cadenA"));
+    }
+
+    @Test
+    public void contarVocales3(){
+        assertEquals(7, Vocales.contarVocales("Java mola MogollOn"));
+    }
+
+    @Test
+    public void contarVocales4(){
+        assertEquals(1, Vocales.contarVocales("4h0r4 s0n num3r0s"));
+    }
+
+    @Test
+    public void contarVocales5(){
+        assertEquals(6, Vocales.contarVocales("Cadena S1n Vocales"));
+    }
 }
