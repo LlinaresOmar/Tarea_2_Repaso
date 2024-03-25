f1fa8f09f68f42459087be61a24a64e4e0af4bd4
OmarLlinares <llinares.omar@gmail.com>
2024-03-25 09:55:22 +0100

    2º Commit Primer Test

--- src/es/iesmz/test/VocalesTest.java
+++ src/es/iesmz/test/VocalesTest.java
@@ -1,6 +1,12 @@
 package es.iesmz.test;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VocalesTest {
+
+    @Test
+    public void contarVocales1(){
+        assertEquals(4, Vocales.contarVocales("Hola Mundo"));
+    }
 
 }
